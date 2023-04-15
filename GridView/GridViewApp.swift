d9d5f0505d68e2394ee71eb563b33b861d56cc54
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-15 20:07:07 +0600

    project init

---
+++ GridView/GridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewApp.swift
+//  GridView
+//
+//  Created by Shah Md Imran Hossain on 15/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
